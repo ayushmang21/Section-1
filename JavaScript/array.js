974f16a33567982dee99a13e81317e71c4d62ced
Ayushman Gupta <146174821+ayushmang21@users.noreply.github.com>
2023-10-27 13:55:17 +0530

    ww

--- JavaScript/array.js
+++ JavaScript/array.js
@@ -19,3 +19,37 @@ console.log( fruits.slice(2, 1000));
 console.log( fruits.slice(2));
 console.log( fruits.slice(1, -3));
 console.log( fruits.slice(0));
+
+// adding element
+
+fruits.push('banana'); //adds at the end
+fruits.unshift('papaya') //adds at the beginning
+
+console.log(fruits);
+
+// replacing elements
+
+fruits[2] = 'cherry';
+console.log(fruits);
+
+//removing elements
+
+fruits.pop(); // removes last element
+fruits.shift(); // removes first element
+console.log(fruits);
+
+//splice parameters - starting index no. , No. of elements to remove
+
+fruits.splice( 2,2 ); // removing elements
+console.log(fruits);
+
+fruits.splice( 1,2 );
+console.log(fruits);
+
+fruits.splice( 1,2, 'tomato', 'berry' );
+console.log(fruits);
+
+//Also used in Strings
+// indexing - []
+//slicing - slice()
+//length - .length
