3c87e9843a74e9dc07a6d6805103df25079b0ae4
Ayushman Gupta <146174821+ayushmang21@users.noreply.github.com>
2023-10-31 13:23:49 +0530

    dfygff

--- JavaScript/array_trav.js
+++ JavaScript/array_trav.js
@@ -11,6 +11,18 @@ for(let n of nums)
 nums.forEach( (n) => { if(n%2 !== 0) console.log(n); } );
 
 // Create a new array that contains the square of all elements
-
+//map
 const newArr = nums.map((n) => { return n**2} );
 console.log(newArr);
+
+//filter
+const evens = nums.filter((a) => { return a%2 === 0 })
+console.log(evens);
+
+const prices = [2300, 150, 1300, 800, 300, 600, 750];
+const p = prices.filter((i) => { return i<1000 && i>500})
+console.log(p);
+
+const mails = ['mmm@yahoo.com', 'abc@gmail.com', 'xyz@gmail.com', 'jjj@hotmail.om']
+const e = mails.filter((m) => { return m.includes('gmail')})
+console.log(e);
